b793afec0db02ea0b4d7a44233675532232acbba
n.shaidulin <n.shaidulin@textback.io>
2022-11-26 11:43:34 +0200

    added screen manager

--- gui/gui.py
+++ gui/gui.py
@@ -1,35 +1,85 @@
 from kivy.clock import Clock
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.video import Video
 from kivymd.app import MDApp
 
 from hardware.temperature import Temperature
 
 
-class App(MDApp):
+class HolotorScreenManager(ScreenManager):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._video = Video(source="resources/countdown_test.mp4", state='play', options={'allow_stretch': True})
+        self.add_widget(LoginScreen(self))
+        self.add_widget(VideoScreen())
+
+
+class LoginScreen(Screen):
+
+    def __init__(self, screen_manager, **kw):
+        super().__init__(**kw)
+        self.name = "Login"
+        self.add_widget(LoginLayout(screen_manager))
+
+
+class VideoScreen(Screen):
+
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.name = "Video"
+        self.add_widget(VideoLayout())
+
+
+class LoginLayout(GridLayout):
+
+    def __init__(self, screen_manager, **kwargs):
+        super().__init__(**kwargs)
+        self.cols = 1
+        self.add_widget(LoginButton(screen_manager))
+
+
+class LoginButton(Button):
+
+    def __init__(self, screen_manager):
+        super().__init__(text="Login", font_size=64)
+        self._sm = screen_manager
+
+    def on_release(self):
+        print("Login process started")
+        self._sm.current = "Video"
+
+
+class VideoLayout(Video):
+
+    def __init__(self):
+        super().__init__(source="resources/countdown_test.mp4", state='play', options={'allow_stretch': True})
         self._video_file_length_seconds = 81
         self._temperature = Temperature()
         self._countdown = 0
         self._current_temperature = -1
 
-    def build(self):
-        self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "BlueGray"
-        Clock.schedule_interval(self.position, 1)
-        return self._video
+    def start(self):
+        Clock.schedule_interval(self._position, 1)
 
-    def position(self, dt):
+    def _position(self, dt):
         if self._countdown == 0:
             self._current_temperature = self._temperature.fetch_temperature()
             self._countdown = 4
-        if self._video.loaded:
+        if self.loaded:
             print(f"Countdown: {self._countdown}; Current CPU temperature: {self._current_temperature}")
-            self._video.seek((100 - self._current_temperature) / self._video_file_length_seconds)
+            self.seek((100 - self._current_temperature) / self._video_file_length_seconds)
         self._countdown -= 1
 
 
+class Holotor(MDApp):
+
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "BlueGray"
+        return HolotorScreenManager()
+
+
 if __name__ == '__main__':
-    App().run()
+    Holotor().run()
